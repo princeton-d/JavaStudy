fc54887ab561e7c53986fa99545f94a6f17290f2
websteak <ssk0080@gmail.com>
2023-01-28 13:49:43 +0900

    조건문

---
+++ src/chap_04/_01_If.java
@@ -0,0 +1,29 @@
+package chap_04;
+
+public class _01_If {
+  public static void main(String[] args) {
+//    조건문
+    int hour = 15;
+    if (hour < 14) {
+      System.out.println("아이스 아메리카노 + 1");
+      System.out.println("샷추가");
+    }
+    System.out.println("커피 주문 완료 #1");
+
+//    오후 2시 이전, 모닝 커피를 마시지 않은 경우
+    hour = 10;
+    boolean morningCoffee = false;
+    if (hour < 14 && morningCoffee == false) {
+      System.out.println("아이스 아메리카노 + 1");
+    }
+    System.out.println("커피 주문 완료 #2");
+
+//    오후 2시 이후이거나 모닝 커피를 마신 경우?
+    hour = 15;
+    morningCoffee = true;
+    if (hour >= 14 || morningCoffee == true) {
+      System.out.println("아이스 아메리카노 (디카페인) +1");
+    }
+    System.out.println("커피 주문 완료 #3");
+  }
+}
