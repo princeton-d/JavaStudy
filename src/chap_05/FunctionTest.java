7e2f5dfd514de92ff2fde2ca6009dd3bcfdfd491
websteak <ssk0080@gmail.com>
2023-01-28 20:45:39 +0900

    1차원 배열 선언

---
+++ src/chap_05/FunctionTest.java
@@ -0,0 +1,15 @@
+package chap_05;
+
+public class FunctionTest {
+  public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 30;
+
+    int sum = addNum(num1, num2);
+    System.out.println(sum);
+  }
+
+  public static int addNum(int n1, int n2) {
+    return n1 + n2;
+  }
+}
