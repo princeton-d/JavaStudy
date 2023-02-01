3073e9943220ae3488fb2a13f000ef03c92df891
websteak <ssk0080@gmail.com>
2023-02-01 20:32:28 +0900

    추상 클래스

--- src/chap_08_Inherit/_03_AccessObjExam.java
+++ src/chap_08_Inherit/_03_AccessObjExam.java
@@ -6,6 +6,6 @@ public class _03_AccessObjExam {
     System.out.println(obj.p);
     System.out.println(obj.p2);
     System.out.println(obj.k);
-//    System.out.println(obj.i);
+//    System.out.println(obj.i); private 은 자기 자신만 접근할 수 있기 때문에 사용 불가
   }
 }
---
+++ src/chap_08_Inherit/_04_AbstractClass.java
@@ -0,0 +1,8 @@
+package chap_08_Inherit;
+// 추상 클래스는 부모로서의 역할만 수행할 수 있다.
+public abstract class _04_AbstractClass {
+  public abstract void sing();
+  public void fly() {
+    System.out.println("날다.");
+  }
+}
