772ea59aeeecc9f9dadae82ccd76703d8530bc7b
websteak <ssk0080@gmail.com>
2023-02-01 19:54:25 +0900

    접근 제한자

---
+++ src/chap_08_Inherit/_03_AccessObjExam.java
@@ -0,0 +1,11 @@
+package chap_08_Inherit;
+
+public class _03_AccessObjExam {
+  public static void main(String[] args) {
+    _03_AccessObj obj = new _03_AccessObj();
+    System.out.println(obj.p);
+    System.out.println(obj.p2);
+    System.out.println(obj.k);
+//    System.out.println(obj.i);
+  }
+}
