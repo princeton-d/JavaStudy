5863e12d297685d717faa9b4ddd2cde5839a4ad6
websteak <ssk0080@gmail.com>
2023-02-01 14:22:01 +0900

    package, inport

---
+++ src/chap_07_Class/_06_PackageTest.java
@@ -0,0 +1,7 @@
+package chap_07_Class;
+
+import chap_07_packageTest.PackageTest;
+
+public class _06_PackageTest {
+  PackageTest test = new PackageTest("hello", 100);
+}
---
+++ src/chap_07_packageTest/PackageTest.java
@@ -0,0 +1,10 @@
+package chap_07_packageTest;
+
+public class PackageTest {
+  String test1;
+  int test2;
+  public PackageTest(String test1, int test2) {
+    this.test1 = test1;
+    this.test2 = test2;
+  }
+}
