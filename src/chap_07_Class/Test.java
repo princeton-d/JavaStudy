38493ed07a17b670c2027de8df48b84f31cce5f6
websteak <ssk0080@gmail.com>
2023-02-06 18:45:30 +0900

    test

--- src/chap_07_Class/Test.java
+++ src/chap_07_Class/Test.java
@@ -3,15 +3,25 @@ package chap_07_Class;
 public class Test {
   static final int WIDTH = 100;
   static final int HEIGHT = 200;
-  public int num;
-  public String kind;
 
-  public Test(int num, String kind) {
+  public final String HART = "hart";
+  public final String DIAMOND = "diamond";
+  public final String CLOVER = "clover";
+  public final String SPADE = "spade";
+
+  public int num;
+  public Kind kind;
+
+  public Test(int num, Kind kind) {
     this.num = num;
     this.kind = kind;
   }
 
   public static void main(String[] args) {
-    Test card = new Test(6, "hart");
+    Test card = new Test(6, Kind.HART);
   }
+}
+
+enum Kind {
+  HART, SPADE, DIAMOND, CLOVER;
 }
