78bc248f51c5c0a3896581c66b36f5c52cc56608
websteak <ssk0080@gmail.com>
2023-02-09 22:41:35 +0900

    실습

---
+++ src/test/CardTest.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.util.Arrays;
+
+public class CardTest {
+  public static void main(String[] args) {
+    Card card = new Card();
+    System.out.println(Arrays.toString(card.deck));
+  }
+}
