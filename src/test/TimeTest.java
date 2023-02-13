c3dc1785138e81701976b9a694b313f054ac34f3
websteak <ssk0080@gmail.com>
2023-02-13 18:56:38 +0900

    instanceof test

--- src/test/TimeTest.java
+++ src/test/TimeTest.java
@@ -1,5 +1,9 @@
 package test;
 
+class Times extends Time {
+
+}
+
 class Time {
   private int hour;
   private int minute;
@@ -24,9 +28,15 @@ class Time {
 public class TimeTest {
   public static void main(String[] args) {
     Time t = new Time();
+    Times tt = new Times();
     t.setHour(21);
     System.out.println(t.getHour());
     t.setHour(100);
     System.out.println(t.getHour());
+    if (t instanceof Times) {
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
   }
 }
