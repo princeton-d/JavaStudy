879403c92fc813a5faadb927f1e1ecf3819dcbd3
websteak <ssk0080@gmail.com>
2023-02-19 18:11:46 +0900

    spring

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -21,21 +21,23 @@ public class HelloController {
   }
 
   @GetMapping("hello-string")
-  @ResponseBody
+  @ResponseBody // http 에 body 부분에 이 데이터를 직접 넣어주겠다는 뜻
   public String helloString(@RequestParam("name") String name) {
-    return "hello " + name;
+    return name;
   }
 
   @GetMapping("hello-api")
   @ResponseBody
-  public Hello helloApi(@RequestParam("name") String name) {
+  public Hello helloApi(@RequestParam("name") String name, @RequestParam("age") int age) {
     Hello hello = new Hello();
     hello.setName(name);
+    hello.setAge(age);
     return hello;
   }
 
   static class Hello {
     private String name;
+    private int age;
 
     public String getName() {
       return name;
@@ -43,6 +45,14 @@ public class HelloController {
 
     public void setName(String name) {
       this.name = name;
+    }
+
+    public int getAge() {
+      return age;
+    }
+
+    public void setAge(int age) {
+      this.age = age;
     }
   }
 }
