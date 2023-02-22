6a8dd4726ff6e5de6883f1a9c9ae0e3e9283e319
websteak <ssk0080@gmail.com>
2023-02-22 21:50:09 +0900

    ArrayList

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -4,6 +4,7 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
@@ -12,8 +13,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-  MemberService memberService = new MemberService();
-  MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+  MemberService memberService;
+  MemoryMemberRepository memberRepository;
+
+  @BeforeEach
+  public void beforeEach() {
+    memberRepository = new MemoryMemberRepository();
+    memberService = new MemberService(memberRepository);
+  }
 
   @AfterEach
   public void afterEach() {
--- src/test/Maps.java
+++ src/test/Maps.java
@@ -1,2 +1,38 @@
-package test;public class Maps {
+package test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Maps {
+  public static void main(String[] args) {
+    Map<Long, String> map = new HashMap<>();
+
+    map.put(1L, "one");
+    map.put(2L, "two");
+//    System.out.println(map.entrySet());
+
+    ArrayList<Integer> list1 = new ArrayList(10);
+    list1.add(new Integer(900));
+    list1.add(new Integer(90));
+    list1.add(new Integer(901));
+    list1.add(new Integer(902));
+    list1.add(new Integer(903));
+    System.out.println(list1);
+
+    ArrayList list2 = new ArrayList(list1.subList(1, 4));
+    System.out.println(list2);
+
+    Collections.sort(list1);
+    System.out.println(list1);
+
+    System.out.println(list1.containsAll(list2));
+
+    list1.add(0, 1);
+    System.out.println(list1.indexOf(1));
+
+    list1.remove(0);
+    System.out.println(list1.get(2));
+  }
 }
