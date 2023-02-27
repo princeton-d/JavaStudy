e8a5151cd58fbfcf995d4e29ee0a06d7949c3e77
websteak <ssk0080@gmail.com>
2023-02-27 19:23:29 +0900

    spring test

---
+++ hello-spring-test/src/main/java/hello/hellospringtest/HelloSpringTestApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringTestApplication.class, args);
+	}
+
+}
---
+++ hello-spring-test/src/test/java/hello/hellospringtest/HelloSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospringtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
