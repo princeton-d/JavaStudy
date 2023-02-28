636e6461e8a969bb7ae95a2f7018e1f790c09da5
websteak <ssk0080@gmail.com>
2023-02-28 18:46:33 +0900

    spring api

--- hello-spring-test/src/main/java/hello/hellospringtest/controller/HelloController.java
+++ hello-spring-test/src/main/java/hello/hellospringtest/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -24,5 +25,51 @@ public class HelloController {
   public String helloMvc(@RequestParam("names") String name, Model model) {
     model.addAttribute("names", name + " webSteak");
     return "hello-template";
+  }
+
+  @GetMapping("hello-string")
+  @ResponseBody
+  public String helloString(@RequestParam("names") String name) {
+    return "hello " + name;
+  }
+
+  @GetMapping("hello-api")
+  @ResponseBody
+  public Hello helloApi(@RequestParam("names") String name, @RequestParam("age") int age,@RequestParam("gender") String gender) {
+    Hello hello = new Hello();
+    hello.setName(name);
+    hello.setAge(age);
+    hello.setGender(gender);
+    return hello;
+  }
+
+  static class Hello {
+    private String name;
+    private int age;
+    private String gender;
+
+    public int getAge() {
+      return age;
+    }
+
+    public void setAge(int age) {
+      this.age = age;
+    }
+
+    public String getGender() {
+      return gender;
+    }
+
+    public void setGender(String gender) {
+      this.gender = gender;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
   }
 }
