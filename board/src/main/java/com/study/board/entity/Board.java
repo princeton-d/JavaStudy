92ab69b57bebefb0f086636d8c00a9c4c7bb11ed
websteak <ssk0080@gmail.com>
2023-03-03 16:46:37 +0900

    게시판 클론코딩

--- board/src/main/java/com/study/board/entity/Board.java
+++ board/src/main/java/com/study/board/entity/Board.java
@@ -16,6 +16,10 @@ public class Board {
 
   private String content;
 
+  private String filename;
+
+  private String filepath;
+
   public Integer getId() {
     return id;
   }
@@ -38,5 +42,21 @@ public class Board {
 
   public void setContent(String content) {
     this.content = content;
+  }
+
+  public String getFilename() {
+    return filename;
+  }
+
+  public void setFilename(String filename) {
+    this.filename = filename;
+  }
+
+  public String getFilepath() {
+    return filepath;
+  }
+
+  public void setFilepath(String filepath) {
+    this.filepath = filepath;
   }
 }
--- board/src/main/java/com/study/board/service/BoardService.java
+++ board/src/main/java/com/study/board/service/BoardService.java
@@ -4,8 +4,11 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -14,7 +17,18 @@ public class BoardService {
   private BoardRepository boardRepository;
 
   // 글 작성 처리
-  public void write(Board board) {
+  public void write(Board board, MultipartFile file) throws Exception {
+
+    String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+    UUID uuid = UUID.randomUUID();
+
+    String fileName = uuid + "_" + file.getOriginalFilename();
+
+    File saveFile = new File(projectPath, fileName);
+
+    file.transferTo(saveFile);
+
     boardRepository.save(board);
   }
 
@@ -28,5 +42,11 @@ public class BoardService {
   public Board boardView(Integer id) {
 
     return boardRepository.findById(id).get();
+  }
+
+  // 특정 게시글 삭제
+  public void boardDelete(Integer id) {
+
+    boardRepository.deleteById(id);
   }
 }
