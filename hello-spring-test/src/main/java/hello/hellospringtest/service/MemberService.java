232acb86b5a5c0c7d52169d324e5c382601c858b
websteak <ssk0080@gmail.com>
2023-03-06 14:54:35 +0900

    spring practice

--- hello-spring-test/src/main/java/hello/hellospringtest/service/MemberService.java
+++ hello-spring-test/src/main/java/hello/hellospringtest/service/MemberService.java
@@ -1,4 +1,38 @@
 package hello.hellospringtest.service;
 
+import hello.hellospringtest.domain.Member;
+import hello.hellospringtest.repository.MemberRepository;
+import hello.hellospringtest.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
+
+  private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+  /**
+   * 회원가 입
+   */
+  public Long join(Member member) {
+    validateDuplicateMember(member); // 중복 회원 검증
+    memberRepository.save(member);
+    return member.getId();
+  }
+
+  private void validateDuplicateMember(Member member) {
+    memberRepository.findByName(member.getName())
+      .ifPresent(m -> {
+        throw new IllegalStateException("이미 존재하는 회원입니다.");
+      });
+  }
+
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
+
+  public Optional<Member> findOne(Long memberId) {
+    return memberRepository.findById(memberId);
+  }
+
 }
