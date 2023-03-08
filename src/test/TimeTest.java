fc76a5b57a1ddff23460f84c89e7b8bf0d39e36e
websteak <ssk0080@gmail.com>
2023-03-08 21:02:37 +0900

    study

--- src/test/TimeTest.java
+++ src/test/TimeTest.java
@@ -3,7 +3,7 @@ package test;
 public class TimeTest {
   public static void main(String[] args) {
     Time time = new Time();
-    time.setHour(25);
+    time.setHour(21);
     time.setMinute(30);
     time.setSecond(58);
 
