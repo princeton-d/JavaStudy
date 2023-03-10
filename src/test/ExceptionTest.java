45598965a224733a213e9fb9b3fd7c0589809812
websteak <ssk0080@gmail.com>
2023-03-10 20:59:53 +0900

    예외처리

---
+++ src/test/ExceptionTest.java
@@ -0,0 +1,18 @@
+package test;
+
+public class ExceptionTest {
+
+  public static void main(String[] args) {
+    int num = 100;
+    int result = 0;
+
+    for (int i = 0; i < 10; i++) {
+      try {
+        result = num / (int) (Math.random() * 10);
+        System.out.println(result);
+      } catch (ArithmeticException e) {
+        System.out.println("0");
+      }
+    }
+  }
+}
