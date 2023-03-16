e5a0918a616d4f9e91c7b1a614c410da6f7e5266
websteak <ssk0080@gmail.com>
2023-03-16 22:44:04 +0900

    ladder

--- ladderGame/src/main/java/org/example/ladder/Ladder.java
+++ ladderGame/src/main/java/org/example/ladder/Ladder.java
@@ -1,11 +1,13 @@
 package org.example.ladder;
 
 import org.example.input.LadderInput;
+import org.example.service.LadderService;
 
 import java.util.Scanner;
 
 public class Ladder {
   private LadderInput ladderInput = new LadderInput();
+  private LadderService ladderService = new LadderService();
 
   public void startGame() {
 
