650e7367fbee3d9224c5769e9ddbf5a184881dc5
princeton <ssk0080@gmail.com>
2023-04-20 15:38:03 +0900

    start moviebuddy

---
+++ moviebuddy/src/main/java/com/moviebuddy/moviebuddy/MoviebuddyApplication.java
@@ -0,0 +1,13 @@
+package com.moviebuddy.moviebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviebuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviebuddyApplication.class, args);
+	}
+
+}
