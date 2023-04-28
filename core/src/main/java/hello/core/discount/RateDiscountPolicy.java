59df758374a7633d4c6b13c30836c9408414b90f
princeton <ssk0080@gmail.com>
2023-04-28 17:44:43 +0900

    정률 할인 정책 인터페이스 및 구현체 생성

---
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    private final int discountPercent = 10;
+    private final int discountAmountZero = 0; // 할인 없음
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        }
+
+        return discountAmountZero;
+    }
+}
