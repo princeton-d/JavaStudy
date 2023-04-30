fc87123073a8dcbbdeb9997a669cd34f3244f621
princeton <ssk0080@gmail.com>
2023-04-30 23:41:14 +0900

    thymeleaf 반복문

--- thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -109,6 +109,13 @@ public class BasicController {
         return "/basic/attribute";
     }
 
+    @GetMapping("/each")
+    public String each(Model model) {
+        addUsers(model);
+
+        return "/basic/each";
+    }
+
     @Data
     static class User {
         private String username;
@@ -118,5 +125,14 @@ public class BasicController {
             this.username = username;
             this.age = age;
         }
+    }
+
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA", 10));
+        list.add(new User("userB", 20));
+        list.add(new User("userC", 30));
+
+        model.addAttribute("users", list);
     }
 }
