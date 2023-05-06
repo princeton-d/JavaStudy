bc639f42ffd40abe8f478752e2f91cca3c0aceb3
princeton <ssk0080@gmail.com>
2023-05-06 21:56:02 +0900

    member service test

--- src/main.java
+++ src/main.java
@@ -4,8 +4,45 @@ import java.util.stream.Stream;
 
 public class main {
     public static void main(String[] args) {
-        String str = "asdfasdfasdfasdfasdf";
 
+        System.out.println(solution(580000));
+    }
+    public static int solution(int price) {
+        int answer = 0;
 
+        if(100000<=price){
+            answer = (int)(price * 0.95);
+        }
+
+        if(300000<=price){
+            answer = (int)(price * 0.90);
+        }
+
+        if(500000<=price){
+            answer = (int)(price * 0.80);
+        }
+
+        if (price < 100000) {
+            answer = price;
+        }
+        return answer;
+    }
+}
+
+class Solution {
+    public int solution(int price) {
+        int answer = 0;
+        if(100000<=price){
+            answer = (int)(price * 0.95);
+        }
+        else if(300000<=price){
+            answer = (int)(price * 0.90);
+        }
+        else if(500000<=price){
+            answer = (int)(price * 0.80);
+        } else {
+            answer = price;
+        }
+        return answer;
     }
 }
