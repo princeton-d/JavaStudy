b01ea7bd6bcf4135cf7f4fd7219cc2b17a77d08d
princeton <ssk0080@gmail.com>
2023-05-11 20:18:08 +0900

    jdbc board init

---
+++ jdbcboard/src/main/java/com/example/jdbcboard/JdbcboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbcboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcboardApplication.class, args);
+    }
+
+}
---
+++ jdbcboard/src/test/java/com/example/jdbcboard/JdbcboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbcboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
