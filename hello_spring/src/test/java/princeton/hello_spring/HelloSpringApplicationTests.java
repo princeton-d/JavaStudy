1043f9075e7e7857a5c3dc804e2d2acfd68c051f
princeton <ssk0080@gmail.com>
2023-05-13 16:20:26 +0900

    hello spring

---
+++ hello_spring/src/test/java/princeton/hello_spring/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package princeton.hello_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
