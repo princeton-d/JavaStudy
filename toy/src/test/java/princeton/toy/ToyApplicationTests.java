e91bdd7ff9701d062fc15ea6e5535a7c9cebd465
princeton <ssk0080@gmail.com>
2023-05-16 22:13:37 +0900

    로그인 처리 실습

---
+++ toy/src/test/java/princeton/toy/ToyApplicationTests.java
@@ -0,0 +1,13 @@
+package princeton.toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
