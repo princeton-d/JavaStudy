57a332efd693a7dd01674ddde93cd70195c72a29
princeton <ssk0080@gmail.com>
2023-05-19 00:34:09 +0900

    User, Article entity setup

--- toy/src/main/java/princeton/toy/domain/User.java
+++ toy/src/main/java/princeton/toy/domain/User.java
@@ -1,31 +1,35 @@
 package princeton.toy.domain;
 
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 public class User {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
+    @Column(name = "user_id")
     private Long id;
-    private String userId;
+
+    private String userLoginId;
     private String userPassword;
     private String username;
+    private LocalDateTime createdAt;
 
-    protected User() {}
+    @OneToMany(mappedBy = "author")
+    private List<Article> articles = new ArrayList<>();
 
-    protected User(Long id, String userId, String userPassword, String username) {
-        this.id = id;
-        this.userId = userId;
-        this.userPassword = userPassword;
-        this.username = username;
+    public void addArticle(Article article) {
+        articles.add(article);
+        article.addAuthor(this);
     }
 }
