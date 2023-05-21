c2866fbe78cafe8dcb595d46a36f7ff91d8c2ac8
princeton <ssk0080@gmail.com>
2023-05-21 16:49:23 +0900

    reset

--- toy/src/main/java/princeton/toy/Application.java
+++ toy/src/main/java/princeton/toy/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToyApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ToyApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- toy/src/test/java/princeton/toy/ApplicationTest.java
+++ toy/src/test/java/princeton/toy/ApplicationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ToyApplicationTests {
+class ApplicationTest {
 
 	@Test
-	void contextLoads() {
+	void ApplicationTests() {
 	}
 
 }
