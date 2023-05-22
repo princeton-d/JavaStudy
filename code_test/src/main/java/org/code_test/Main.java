abc93db5609adaaf8c48b018c4dd8f01085a3cf2
princeton <ssk0080@gmail.com>
2023-05-22 21:16:08 +0900

    usersRepository 테스트 코드 작성

--- code_test/src/main/java/org/code_test/Main.java
+++ code_test/src/main/java/org/code_test/Main.java
@@ -1,16 +1,29 @@
 package org.code_test;
 
-import java.time.LocalDateTime;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Member member = new Member.Builder()
-                .id(1L)
-                .name("박보검")
-                .createdAt(LocalDateTime.now())
-                .address("대구광역시 동구 서호동")
-                .password("password")
-                .email("princeton@gmail.com")
-                .build();
+
+    public static void main(String[] args) throws IOException {
+//        System.setIn(new FileInputStream("src/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String input = br.readLine();
+        String[] splitInput = input.split(" ");
+        int A = Integer.parseInt(splitInput[0]);
+        int B = Integer.parseInt(splitInput[1]);
+        int V = Integer.parseInt(splitInput[2]);
+
+        int result = (V - B) / (A - B);
+        if ((V - B) % (A - B) != 0) {
+            result++;
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
     }
+
 }
--- toy/src/test/java/princeton/toy/repository/UsersRepositoryTest.java
+++ toy/src/test/java/princeton/toy/repository/UsersRepositoryTest.java
@@ -1,10 +1,21 @@
 package princeton.toy.repository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.transaction.annotation.Transactional;
+import princeton.toy.domain.Users;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 @SpringBootTest
 @Transactional
 class UsersRepositoryTest {
@@ -13,12 +24,138 @@ class UsersRepositoryTest {
     UsersRepository usersRepository;
 
     @Test
+    @DisplayName("유저 정보 저장 테스트")
     public void save() throws Exception {
         //given
+        Users user = Users.builder()
+                .email("princeton@gmail.com")
+                .createdAt(LocalDateTime.now())
+                .nickname("princeton")
+                .password("password")
+                .articles(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
 
         //when
+        Long savedId = usersRepository.save(user);
 
         //then
+        assertThat(savedId).isEqualTo(user.getId());
+    }
+
+    @Test
+    @DisplayName("중복된 닉네임 회원가입 테스트")
+    public void duplicateUserSave() throws Exception {
+        //given
+        Users userA = Users.builder()
+                .email("princeton1@gmail.com")
+                .createdAt(LocalDateTime.now())
+                .nickname("princeton")
+                .password("password1")
+                .articles(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
+        usersRepository.save(userA);
+        log.info("userA.getId={}", userA.getId());
+
+        Users userB = Users.builder()
+                .email("princeton2@gmail.com")
+                .createdAt(LocalDateTime.now())
+                .nickname("princeton")
+                .password("password2")
+                .articles(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
+
+        //when, then
+        assertThatThrownBy(() -> usersRepository.save(userB))
+                .isInstanceOf(DataIntegrityViolationException.class)
+                .hasMessage("이미 존재하는 닉네임입니다.");
+    }
+
+    @Test
+    @DisplayName("id로 유저 조회")
+    public void findById() throws Exception {
+        //given
+        Users user = Users.builder()
+                .email("princeton@gmail.com")
+                .createdAt(LocalDateTime.now())
+                .nickname("princeton")
+                .password("password")
+                .articles(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
+        Long savedId = usersRepository.save(user);
+
+        //when
+        Users findUser = usersRepository.findById(savedId);
+
+        //then
+        assertThat(findUser.getId()).isEqualTo(savedId);
+    }
+
+    @Test
+    @DisplayName("닉네임으로 유저 조회")
+    public void findByNickname() throws Exception {
+        //given
+        String nickname = "princeton";
+        Users user = Users.builder()
+                .email("princeton@gmail.com")
+                .createdAt(LocalDateTime.now())
+                .nickname(nickname)
+                .password("password")
+                .articles(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
+        Long savedId = usersRepository.save(user);
+
+        //when
+        Users findUser = usersRepository.findByNickname(nickname).get(0);
+
+        //then
+        assertThat(findUser.getNickname()).isEqualTo("princeton");
+        assertThat(findUser.getId()).isEqualTo(savedId);
+    }
+
+    @Test
+    @DisplayName("전체 유저 조회")
+    public void findAll() throws Exception {
+        //given
+        Users userA = Users.builder()
+                .email("princetonA@gmail.com")
+                .createdAt(LocalDateTime.now())
+                .nickname("princetonA")
+                .password("passwordA")
+                .articles(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
+        usersRepository.save(userA);
+
+        Users userB = Users.builder()
+                .email("princetonB@gmail.com")
+                .createdAt(LocalDateTime.now())
+                .nickname("princetonB")
+                .password("passwordB")
+                .articles(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
+        usersRepository.save(userB);
+
+        Users userC = Users.builder()
+                .email("princetonC@gmail.com")
+                .createdAt(LocalDateTime.now())
+                .nickname("princetonC")
+                .password("passwordC")
+                .articles(new ArrayList<>())
+                .comments(new ArrayList<>())
+                .build();
+        usersRepository.save(userC);
+
+        //when
+        List<Users> findUsers = usersRepository.findAll();
+
+        //then
+        assertThat(findUsers.size()).isEqualTo(3);
     }
 
 }
