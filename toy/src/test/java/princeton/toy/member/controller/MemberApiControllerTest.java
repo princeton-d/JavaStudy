d8e15f57ce1f22a74d70ac63a1a777b26bc2c11b
princeton <ssk0080@gmail.com>
2023-06-07 23:58:12 +0900

    .

--- toy/src/test/java/princeton/toy/member/controller/MemberApiControllerTest.java
+++ toy/src/test/java/princeton/toy/member/controller/MemberApiControllerTest.java
@@ -1,10 +1,6 @@
 package princeton.toy.member.controller;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest
 class MemberApiControllerTest {
