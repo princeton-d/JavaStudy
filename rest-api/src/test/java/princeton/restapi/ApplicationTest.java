a2fc5bee28d2efd598e132fe58c52c9ccc004073
princeton <ssk0080@gmail.com>
2023-06-21 10:16:55 +0900

    api study

---
+++ rest-api/src/test/java/princeton/restapi/ApplicationTest.java
@@ -0,0 +1,13 @@
+package princeton.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
