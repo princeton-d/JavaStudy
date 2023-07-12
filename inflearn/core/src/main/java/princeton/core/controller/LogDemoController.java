3331d17b022c9802769c48c0da5984e33cb8bcbb
princeton <ssk0080@gmail.com>
2023-07-12 18:42:47 +0900

    scope proxy

--- inflearn/core/src/main/java/princeton/core/controller/LogDemoController.java
+++ inflearn/core/src/main/java/princeton/core/controller/LogDemoController.java
@@ -2,7 +2,6 @@ package princeton.core.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,12 +13,11 @@ import princeton.core.service.LogDemoService;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
         myLogger.setRequestURL(requestURL);
 
