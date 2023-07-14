87f8bd0c354c7594c24b3480ccf53f735e6b2fd9
princeton <ssk0080@gmail.com>
2023-07-14 22:13:55 +0900

    filter

---
+++ inflearn/mvc2/src/main/java/princeton/mvc2/filter/LoginCheckFilter.java
@@ -0,0 +1,35 @@
+package princeton.mvc2.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginCheckFilter implements Filter {
+
+    private static final String[] whitelist = {"/", "/members/add", "/login", "/css/*"};
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String requestURI = httpRequest.getRequestURI();
+
+        try {
+            log.info("인증 체크 필터 시작 {}", requestURI);
+            if (isLoginCheckPath(requestURI)) {
+                log.info("인증 체크 로직 실행 {}", requestURI);
+            }
+        } catch () {
+        }
+    }
+
+    private boolean isLoginCheckPath(String requestURI) {
+        return !PatternMatchUtils.simpleMatch(whitelist, requestURI);
+    }
+
+}
--- inflearn/post-api/src/main/java/princeton/postapi/repository/ItemRepository.java
+++
@@ -1,7 +0,0 @@
-package princeton.postapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import princeton.postapi.domain.entity.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
