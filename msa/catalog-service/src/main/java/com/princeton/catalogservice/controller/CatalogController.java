1b76aefced92179807c9bd022bb02eebfad835b6
kimdonghyun <ssk0080@gmail.com>
2023-08-11 22:13:03 +0900

    커넥트 설정

--- msa/catalog-service/src/main/java/com/princeton/catalogservice/controller/CatalogController.java
+++ msa/catalog-service/src/main/java/com/princeton/catalogservice/controller/CatalogController.java
@@ -4,12 +4,16 @@ import com.princeton.catalogservice.dto.response.ResponseCatalogDto;
 import com.princeton.catalogservice.global.response.ResponseDataDto;
 import com.princeton.catalogservice.service.CatalogService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -27,6 +31,14 @@ public class CatalogController {
         return ResponseEntity
                 .status(OK)
                 .body(new ResponseDataDto<>(responseCatalogDtos));
+    }
+
+    @GetMapping("/catalogs/{testNumber}")
+    public ResponseEntity<ResponseDataDto<Map<Integer, String>>> getTest(@PathVariable Integer testNumber) {
+        Map<Integer, String> testMap = new HashMap<>();
+        testMap.put(testNumber, "testValue");
+
+        return ResponseEntity.status(OK).body(new ResponseDataDto<>(testMap));
     }
 
 }
--- msa/user-service/src/main/java/com/princeton/userservice/service/MemberServiceImpl.java
+++ msa/user-service/src/main/java/com/princeton/userservice/service/MemberServiceImpl.java
@@ -3,20 +3,33 @@ package com.princeton.userservice.service;
 import com.princeton.userservice.dto.Response.ResponseMemberSignupResultDto;
 import com.princeton.userservice.dto.request.RequestMemberSignupDto;
 import com.princeton.userservice.entity.Member;
+import com.princeton.userservice.global.response.ResponseDataDto;
 import com.princeton.userservice.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import static com.princeton.userservice.mapper.MemberMapper.*;
+import static org.springframework.http.HttpMethod.*;
 
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
+
+    private final Environment environment;
+
+    private final RestTemplate restTemplate;
 
     @Override
     @Transactional
@@ -25,6 +38,15 @@ public class MemberServiceImpl implements MemberService {
 
         Member member = INSTANCE.requestMemberSignupDtoToEntity(requestMemberSignupDto, userId);
         Member savedMember = memberRepository.save(member);
+
+        String catalogUrl = String.format(environment.getProperty("catalog-service.test-url"), 134);
+
+        ResponseDataDto<HashMap<Integer, String>> body = restTemplate.exchange(catalogUrl, GET, null, new ParameterizedTypeReference<ResponseDataDto<HashMap<Integer, String>>>() {
+        }).getBody();
+
+        HashMap<Integer, String> data = body.getData();
+
+        System.out.println(data.toString());
 
         return INSTANCE.entityToResponseMemberSignupResultDto(savedMember);
     }
